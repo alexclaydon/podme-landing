9c1a574600cf5b2ee6f9cab1f88c94259b7a4ffc
Alex Claydon <alexclaydon@gmail.com>
2021-04-28 06:31:12 +0000

    feat: Content updates

--- pages/contact.js
+++ pages/contact.js
@@ -20,7 +20,7 @@ export default function Contact() {
         <main className="overflow-hidden">
           {/* Header */}
           <div className="bg-gray-50">
-            <div className="py-24 lg:py-32">
+            <div className="py-6">
               <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
                   Get in touch
@@ -35,9 +35,9 @@ export default function Contact() {
 
           {/* Contact section */}
           <section className="relative bg-white" aria-labelledby="contactHeading">
-            <div className="bg-gray-100">
-              <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-                <div className="relative bg-white shadow-xl">
+            <div>
+              <div className="max-w-7xl mx-auto px-4 sm:py-8 sm:px-6 lg:px-8">
+                <div className="relative bg-gray-100 shadow-xl">
                   <h2 className="sr-only">Contact us</h2>
 
                   <div className="grid grid-cols-1 lg:grid-cols-3">
--- pages/index.js
+++ pages/index.js
@@ -103,28 +103,11 @@ export default function Home() {
                 </div>
                 <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                   <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                    <span className="block text-white">Remote practice management</span>
-                    <span className="block text-indigo-200">you can trust</span>
+                    <span className="block text-white">Poddle.</span>
                   </h1>
                   <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
                     Secure, fast and reliable video calling for health practitioners.
                   </p>
-                  <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-                    <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                      <a
-                        href="#"
-                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
-                      >
-                        Get started
-                      </a>
-                      <a
-                        href="#"
-                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
-                      >
-                        Live demo
-                      </a>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
