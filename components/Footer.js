5941569f8424433599d16e2cadcac760c7ce9bdd
Alex Claydon <alexclaydon@gmail.com>
2021-04-28 03:53:59 +0000

    feat: Updated footer content

--- components/Footer.js
+++ components/Footer.js
@@ -54,7 +54,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-400">
-            &copy; 2020 Workflow, Inc. All rights reserved.
+            &copy; 2020 Poddle, Pty. Ltd. rights reserved.
           </p>
         </div>
       </div>
