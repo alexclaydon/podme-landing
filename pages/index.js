98532cf66dcccfdf4cb251467d0e424ca6efbae0
Alex Claydon <alexclaydon@gmail.com>
2021-04-28 06:08:49 +0000

    feat: Removed redundant commented sections

--- pages/index.js
+++ pages/index.js
@@ -72,32 +72,6 @@ const features = [
     icon: CheckIcon,
   },
 ]
-// const metrics = [
-//   {
-//     id: 1,
-//     stat: '>500',
-//     emphasis: 'Sessions',
-//     rest: 'completed in beta to optimise the platform.',
-//   },
-//   {
-//     id: 2,
-//     stat: '99%',
-//     emphasis: 'Client',
-//     rest: 'satisfaction.',
-//   },
-//   {
-//     id: 3,
-//     stat: '1',
-//     emphasis: 'Link',
-//     rest: 'stable over time.',
-//   },
-//   {
-//     id: 4,
-//     stat: '$10',
-//     emphasis: 'Issues resolved',
-//     rest: 'lacus nibh integer quis.',
-//   },
-// ]
 
 export default function Home() {
   return (
@@ -155,52 +129,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-          {/* #TODO: Logo Cloud - Disabled for MVP but specific users to be added in time for production launch. */}
-          {/* <div className="bg-gray-100">
-            <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-              <p className="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
-                Trusted by over 5 very average small businesses
-              </p>
-              <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-                <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-                    alt="Tuple"
-                  />
-                </div>
-                <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-                    alt="Mirage"
-                  />
-                </div>
-                <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-                    alt="StaticKit"
-                  />
-                </div>
-                <div className="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-                    alt="Transistor"
-                  />
-                </div>
-                <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-                    alt="Workcation"
-                  />
-                </div>
-              </div>
-            </div>
-          </div> */}
 
           {/* Alternating Feature Sections */}
           <div className="relative pt-16 pb-32 overflow-hidden">
@@ -343,53 +271,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-          {/* #TODO: Stats section - currently hidden although may restore */}
-          {/* <div className="relative bg-gray-900">
-            <div className="h-80 absolute inset-x-0 bottom-0 xl:top-0 xl:h-full">
-              <div className="h-full w-full xl:grid xl:grid-cols-2">
-                <div className="h-full xl:relative xl:col-start-2">
-                  <img
-                    className="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0"
-                    src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                    alt="People working on laptops"
-                  />
-                  <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8">
-              <div className="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
-                <h2 className="text-sm font-semibold tracking-wide uppercase">
-                  <span className="bg-gradient-to-r from-purple-300 to-indigo-300 bg-clip-text text-transparent">
-                    Valuable Metrics
-                  </span>
-                </h2>
-                <p className="mt-3 text-3xl font-extrabold text-white">
-                  Get actionable data that will help grow your business
-                </p>
-                <p className="mt-5 text-lg text-gray-300">
-                  Rhoncus sagittis risus arcu erat lectus bibendum. Ut in adipiscing quis in viverra
-                  tristique sem. Ornare feugiat viverra eleifend fusce orci in quis amet. Sit in et
-                  vitae tortor, massa. Dapibus laoreet amet lacus nibh integer quis. Eu vulputate
-                  diam sit tellus quis at.
-                </p>
-                <div className="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2">
-                  {metrics.map((item) => (
-                    <p key={item.id}>
-                      <span className="block text-2xl font-bold text-white">{item.stat}</span>
-                      <span className="mt-1 block text-base text-gray-300">
-                        <span className="font-medium text-white">{item.emphasis}</span> {item.rest}
-                      </span>
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div> */}
 
           {/* #TODO: Email notification section: To be used to allow people to register during the beta period to receive notification of our public launch date; following public launch, to be replaced with `Trial` component */}
           <LaunchNotify />
